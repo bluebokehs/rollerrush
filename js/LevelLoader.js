0a4f132c7141eb81b4331af6c8dcb074df135f27
Nathan Koehler <nathankoehle@gmail.com>
2021-12-06 22:35:50 -0500

    working on the level

--- js/LevelLoader.js
+++ js/LevelLoader.js
@@ -1,23 +1,23 @@
-import * as THREE from "./three.js";
 import * as CANNON from "./cannon-es.js";
 import { OBJLoader } from "https://cdn.skypack.dev/three@0.131.1/examples/jsm/loaders/OBJLoader.js";
 
 function loadLevel(gameData) {
-    const objLoader = new OBJLoader();
-
-    switch(gameData.gameLevel) {
+    switch (gameData.gameLevel) {
         case 1:
-            gameData.gameLevel = "../maps/level5.obj"
+            gameData = loadFirstMap(gameData);
             break;
         case 2:
-            gameData.gameLevel = "../maps/level6.obj"
+            gameData = loadSecondMap(gameData);
             break;
-        default:
-            gameData.gameLevel = "../maps/level5.obj"
     }
 
+    return gameData;
+}
+
+function loadFirstMap(gameData) {
+    const objLoader = new OBJLoader();
     objLoader.load(
-        gameData.gameLevel,
+        "../maps/level6/inner_ring.obj",
         (object) => {
             gameData.gameScene.add(object);
 
@@ -30,12 +30,14 @@ function loadLevel(gameData) {
             const indices = Object.keys(vertices).map(Number);
             const levelShape = new CANNON.Trimesh(vertices, indices);
 
-            gameData.gameLevelBody.addShape(levelShape);
-            gameData.gameLevelBody.position.x = levelMesh.position.x;
-            gameData.gameLevelBody.position.y = levelMesh.position.y;
-            gameData.gameLevelBody.position.z = levelMesh.position.z;
+            let levelBody = new CANNON.Body({ mass: 0, material: new CANNON.Material("groundMaterial") });
 
-            gameData.gameLevelMesh = levelMesh;
+            levelBody.addShape(levelShape);
+            levelBody.position.x = levelMesh.position.x;
+            levelBody.position.y = levelMesh.position.y;
+            levelBody.position.z = levelMesh.position.z;
+
+            gameData.gameWorld.addBody(levelBody);
             gameData.gameLevelLoaded = true;
         },
         (xhr) => {
@@ -45,6 +47,91 @@ function loadLevel(gameData) {
             console.log("An error happened " + error);
         }
     );
+    return gameData;
+}
+
+function loadSecondMap(gameData) {
+    const objLoader = new OBJLoader();
+    let objects = [
+        "../maps/level6/outer_ring.obj",
+        "../maps/level6/inner_ring.obj",
+    ];
+    objects.forEach((model) => {
+        objLoader.load(
+            model,
+            (object) => {
+                gameData.gameScene.add(object);
+
+                let levelMesh = object.children[0];
+                levelMesh.material = gameData.gameLevelMaterial;
+                levelMesh.position.x = 0;
+                levelMesh.position.y = -3;
+
+                const vertices = levelMesh.geometry.attributes.position.array;
+                const indices = Object.keys(vertices).map(Number);
+                const levelShape = new CANNON.Trimesh(vertices, indices);
+
+                let levelBody = new CANNON.Body({ mass: 0, material: new CANNON.Material("groundMaterial") });
+
+                levelBody.addShape(levelShape);
+                levelBody.position.x = levelMesh.position.x;
+                levelBody.position.y = levelMesh.position.y;
+                levelBody.position.z = levelMesh.position.z;
+
+                gameData.gameWorld.addBody(levelBody);
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+            },
+            (error) => {
+                console.log("An error happened " + error);
+            }
+        );
+    });
+
+    let droppable = ["../maps/level6/box2.obj", "../maps/level6/box3.obj"];
+    droppable.forEach((model) => {
+        for (let i = 0; i < 60; i++) {
+            objLoader.load(
+                model,
+                (object) => {
+                    gameData.gameScene.add(object);
+    
+                    let levelMesh = object.children[0];
+                    levelMesh.material = gameData.gameLevelMaterial;
+                    levelMesh.position.x = 0;
+                    levelMesh.position.y = -3;
+                    levelMesh.rotateOnWorldAxis(new THREE.Vector3(0, 1, 0), THREE.Math.degToRad(i * 6));
+    
+                    const vertices = levelMesh.geometry.attributes.position.array;
+                    const indices = Object.keys(vertices).map(Number);
+                    const levelShape = new CANNON.Trimesh(vertices, indices);
+    
+                    let levelBody = new CANNON.Body({ mass: 0, material: new CANNON.Material("groundMaterial") });
+    
+                    levelBody.addShape(levelShape);
+                    levelBody.position.x = levelMesh.position.x;
+                    levelBody.position.y = levelMesh.position.y;
+                    levelBody.position.z = levelMesh.position.z;
+                    levelBody.quaternion.x = levelMesh.quaternion.x;
+                    levelBody.quaternion.y = levelMesh.quaternion.y;
+                    levelBody.quaternion.z = levelMesh.quaternion.z;
+                    levelBody.quaternion.w = levelMesh.quaternion.w;
+    
+                    gameData.gameWorld.addBody(levelBody);
+                },
+                (xhr) => {
+                    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+                },
+                (error) => {
+                    console.log("An error happened " + error);
+                }
+            );
+        }
+        
+    });
+    gameData.gameLevelLoaded = true;
+
     return gameData;
 }
 
